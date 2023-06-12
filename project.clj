ffe46e0fa074572ccd4f8900624c57da4ae85b1d
Kevin Lalumiere <klalumiere@flyingk43.local>
2023-06-11 22:03:10 -0400

    Add templating library

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
   :dependencies [[compojure "1.7.0"]
                  [org.clojure/clojure "1.11.1"]
                  [ring/ring-core "1.9.6"]
-                 [ring/ring-jetty-adapter "1.9.6"]]
+                 [ring/ring-jetty-adapter "1.9.6"]
+                 [selmer "1.12.58"]]
   :plugins [[lein-ring "0.12.6"]]
   :ring {:handler cv-creator.server/app :port 8080}
   :repl-options {:init-ns cv-creator.server}
--- src/cv_creator/section_html_renderer.clj
+++ src/cv_creator/section_html_renderer.clj
@@ -1,8 +1,10 @@
 (ns cv-creator.section-html-renderer
   (:require
    [cv-creator.section]
-   [cv-creator.html-renderer])
+   [cv-creator.html-renderer]
+   [selmer.parser :as selmer])
   (:import [cv_creator.section HeadSection]))
 
 (extend-type HeadSection cv-creator.html-renderer/HtmlRenderer
-             (render-html [this] (get this :name)))
+             (render-html [this] (selmer/render "yo
+{{name}}" this)))
