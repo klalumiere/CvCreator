efe4bcbab8b9d0d55bfe0d50bf9a00a9eaaad712
Kevin Lalumiere <kevin.lalumiere@gmail.com>
2023-06-18 19:39:34 -0400

    Reformat

--- src/cv_creator/section_html_renderer.clj
+++ src/cv_creator/section_html_renderer.clj
@@ -17,20 +17,6 @@
             WebPageItem]))
 
 
-(extend-type ExperienceItem cv-creator.html-renderer/HtmlRenderer
-             (render-html [this] (selmer/render "<table>
-<tr>
-<td align=\"left\"><strong>{{title}}</strong>
-</td>
-<td align=\"right\">{{date}}</td>
-</tr>
-<tr>
-<td colspan=\"2\" align=\"left\">{{business}}</td>
-</tr>
-</table><hr><ul>{{rendered-subitems|safe}}</ul><br>" (assoc this :rendered-subitems
-                                                            (cv-creator.html-renderer/render-html-all (:subitems this))))))
-
-
 (extend-type EducationItem cv-creator.html-renderer/HtmlRenderer
              (render-html [this] (selmer/render "<table>
 <tr>
@@ -45,36 +31,19 @@
                                                         (cv-creator.html-renderer/render-html-all (:subitems this))))))
 
 
-(extend-type SubitemEducation cv-creator.html-renderer/HtmlRenderer
-             (render-html [this] (selmer/render "<li><strong>{{label}}: </strong>{{subitem}}</li>" this)))
-
-
-(extend-type Section cv-creator.html-renderer/HtmlRenderer
-             (render-html [this] (if (empty? (:items this))
-                                   ""
-                                   (selmer/render "<br><strong style=\"font-size: 125%;\">{{label}}</strong>
-<hr class=\"section\">
-<ul>{{rendered-items|safe}}</ul><br>" (assoc this :rendered-items (cv-creator.html-renderer/render-html-all (:items this)))))))
-
-
-(extend-type Item cv-creator.html-renderer/HtmlRenderer
-             (render-html [this] (if (string/blank? (:item this))
-                                   ""
-                                   (selmer/render "<li>{{item}}</li><ul>{{rendered-subitems|safe}}</ul>
-" (assoc this :rendered-subitems (cv-creator.html-renderer/render-html-all (:subitems this)))))))
-
-
-(extend-type PhoneItem cv-creator.html-renderer/HtmlRenderer
-             (render-html [this] (if (string/blank? (:item this))
-                                   ""
-                                   (selmer/render "<td style=\"text-align: right\">{{label}}: {{item}}</td>" this))))
-
-
-(extend-type WebPageItem cv-creator.html-renderer/HtmlRenderer
-             (render-html [this] (if (string/blank? (:item this))
-                                   ""
-                                   (selmer/render "<tr>
-<td colspan=\"2\">{{label}}: <a href=\"{{item}}\">{{item}}</a> </td></tr>" this))))
+(extend-type ExperienceItem cv-creator.html-renderer/HtmlRenderer
+             (render-html [this] (selmer/render "<table>
+<tr>
+<td align=\"left\"><strong>{{title}}</strong>
+</td>
+<td align=\"right\">{{date}}</td>
+</tr>
+<tr>
+<td colspan=\"2\" align=\"left\">{{business}}</td>
+</tr>
+</table><hr><ul>{{rendered-subitems|safe}}</ul>
+<br>" (assoc this :rendered-subitems
+             (cv-creator.html-renderer/render-html-all (:subitems this))))))
 
 
 (extend-type HeadSection cv-creator.html-renderer/HtmlRenderer
@@ -95,3 +64,36 @@
 </table>" (assoc this
                  :rendered-phone (cv-creator.html-renderer/render-html (:phone this))
                  :rendered-webpage (cv-creator.html-renderer/render-html (:web-page this))))))
+
+
+(extend-type Item cv-creator.html-renderer/HtmlRenderer
+             (render-html [this] (if (string/blank? (:item this))
+                                   ""
+                                   (selmer/render "<li>{{item}}</li><ul>{{rendered-subitems|safe}}</ul>
+" (assoc this :rendered-subitems (cv-creator.html-renderer/render-html-all (:subitems this)))))))
+
+
+(extend-type PhoneItem cv-creator.html-renderer/HtmlRenderer
+             (render-html [this] (if (string/blank? (:item this))
+                                   ""
+                                   (selmer/render "<td style=\"text-align: right\">{{label}}: {{item}}</td>" this))))
+
+
+(extend-type Section cv-creator.html-renderer/HtmlRenderer
+             (render-html [this] (if (empty? (:items this))
+                                   ""
+                                   (selmer/render "<br><strong style=\"font-size: 125%;\">{{label}}</strong>
+<hr class=\"section\">
+<ul>{{rendered-items|safe}}</ul>
+<br>" (assoc this :rendered-items (cv-creator.html-renderer/render-html-all (:items this)))))))
+
+
+(extend-type SubitemEducation cv-creator.html-renderer/HtmlRenderer
+             (render-html [this] (selmer/render "<li><strong>{{label}}: </strong>{{subitem}}</li>" this)))
+
+
+(extend-type WebPageItem cv-creator.html-renderer/HtmlRenderer
+             (render-html [this] (if (string/blank? (:item this))
+                                   ""
+                                   (selmer/render "<tr>
+<td colspan=\"2\">{{label}}: <a href=\"{{item}}\">{{item}}</a> </td></tr>" this))))
--- test/cv_creator/core_test.clj
+++ test/cv_creator/core_test.clj
@@ -9,54 +9,50 @@
 
 
 (def a-phone-item (cv-creator.section/map->PhoneItem {:label "Phone"
-                                                              :item "(023) 456-7891"}))
+                                                      :item "(023) 456-7891"}))
 (def a-section-label "arbitrary")
 (def a-subitem-education (cv-creator.section/map->SubitemEducation {:label "Thesis"
-                                                                            :subitem "How to have fun"}))
+                                                                    :subitem "How to have fun"}))
 (def a-web-page-item (cv-creator.section/map->WebPageItem {:label "Web page"
-                                                                   :item "https://alain.terieur.com"}))
+                                                           :item "https://alain.terieur.com"}))
 (def an-item (cv-creator.section/map->Item {:item "An item"}))
 
 
 (def an-education-item (cv-creator.section/map->EducationItem {:degree "PhD"
-                                                                      :school "UdeS"
-                                                                      :date "2015"
-                                                                      :subitems [a-subitem-education]}))
+                                                               :school "UdeS"
+                                                               :date "2015"
+                                                               :subitems [a-subitem-education]}))
 (def an-experience-item (cv-creator.section/map->ExperienceItem {:title "Master"
-                                                               :business "Jedi School"
-                                                               :date "2017"
-                                                               :subitems [an-item]}))
+                                                                 :business "Jedi School"
+                                                                 :date "2017"
+                                                                 :subitems [an-item]}))
 (def an-head-section (cv-creator.section/map->HeadSection {:name "Alain Térieur"
-                                                                  :e-mail "alain.térieur@gmaille.com"
-                                                                  :address-door "123 Street"
-                                                                  :address-town "Montréal (Qc), Canada, H2T 2F6"
-                                                                  :web-page a-web-page-item
-                                                                  :phone a-phone-item}))
+                                                           :e-mail "alain.térieur@gmaille.com"
+                                                           :address-door "123 Street"
+                                                           :address-town "Montréal (Qc), Canada, H2T 2F6"
+                                                           :web-page a-web-page-item
+                                                           :phone a-phone-item}))
 (def an-item-with-subitems (cv-creator.section/map->Item {:item "An item with subitems"
-                                                                 :subitems [an-item an-item]}))
+                                                          :subitems [an-item an-item]}))
 
 
 (def a-section (cv-creator.section/map->Section {:label a-section-label
-                                                         :items [an-item-with-subitems]}))
+                                                 :items [an-item-with-subitems]}))
 
 
 (test/deftest html-renderer
 
   (test/testing "render-html ExperienceItem is not empty"
     (test/is (not (string/blank?
-                   (cv-creator.html-renderer/render-html
-                    an-experience-item)))))
-
+                   (cv-creator.html-renderer/render-html  an-experience-item)))))
 
   (test/testing "render-html EducationItem is not empty"
     (test/is (not (string/blank?
-                   (cv-creator.html-renderer/render-html
-                    an-education-item)))))
+                   (cv-creator.html-renderer/render-html an-education-item)))))
 
   (test/testing "render-html SubitemEducation is not empty"
     (test/is (not (string/blank?
-                   (cv-creator.html-renderer/render-html
-                    a-subitem-education)))))
+                   (cv-creator.html-renderer/render-html a-subitem-education)))))
 
   (test/testing "render-html-all is not empty when collection is not"
     (test/is (not (string/blank?
@@ -68,50 +64,41 @@
 
   (test/testing "render-html Section is not empty when items are not"
     (test/is (not (string/blank?
-                   (cv-creator.html-renderer/render-html
-                    a-section)))))
+                   (cv-creator.html-renderer/render-html a-section)))))
 
   (test/testing "render-html Section is empty when items are empty"
     (test/is (string/blank?
-              (cv-creator.html-renderer/render-html
-               (cv-creator.section/map->Section {:items []})))))
+              (cv-creator.html-renderer/render-html (cv-creator.section/map->Section {:items []})))))
 
   (test/testing "render-html Item is empty if item is"
     (test/is (string/blank?
-              (cv-creator.html-renderer/render-html
-               (cv-creator.section/map->Item {:item ""})))))
+              (cv-creator.html-renderer/render-html (cv-creator.section/map->Item {:item ""})))))
 
   (test/testing "render-html Item is not empty"
     (test/is (not (string/blank?
-                   (cv-creator.html-renderer/render-html
-                    an-item-with-subitems)))))
+                   (cv-creator.html-renderer/render-html an-item-with-subitems)))))
 
   (test/testing "render-html PhoneItem is not empty when a phone number is provided"
     (test/is (not (string/blank?
-                   (cv-creator.html-renderer/render-html
-                    a-phone-item)))))
+                   (cv-creator.html-renderer/render-html a-phone-item)))))
 
   (test/testing "render-html PhoneItem is empty when no phone number is provided"
     (test/is (string/blank?
-              (cv-creator.html-renderer/render-html
-               (cv-creator.section/map->PhoneItem {:label "Phone"
-                                                   :item ""})))))
+              (cv-creator.html-renderer/render-html (cv-creator.section/map->PhoneItem {:label "Phone"
+                                                                                        :item ""})))))
 
   (test/testing "render-html WebPageItem is not empty when a web page is provided"
     (test/is (not (string/blank?
-                   (cv-creator.html-renderer/render-html
-                    a-web-page-item)))))
+                   (cv-creator.html-renderer/render-html a-web-page-item)))))
 
   (test/testing "render-html WebPageItem is empty when no web page is provided"
     (test/is (string/blank?
-              (cv-creator.html-renderer/render-html
-               (cv-creator.section/map->WebPageItem {:label "Web page"
-                                                     :item ""})))))
+              (cv-creator.html-renderer/render-html (cv-creator.section/map->WebPageItem {:label "Web page"
+                                                                                          :item ""})))))
 
   (test/testing "render-html HeadSection is not empty"
     (test/is (not (string/blank?
-                   (cv-creator.html-renderer/render-html
-                    an-head-section)))))
+                   (cv-creator.html-renderer/render-html an-head-section)))))
 
   (test/testing "create-html is not empty"
     (test/is (not (string/blank?
