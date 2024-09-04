53b6cd4518077d5b433dce506458f4f92160e54a
Kevin Lalumiere <kevin.lalumiere@gmail.com>
2024-09-04 00:12:31 -0400

    Add create-cv

--- src/cv_creator/core.clj
+++ src/cv_creator/core.clj
@@ -5,7 +5,9 @@
    [cv-creator.section-html-renderer]
    [cv-creator.section]))
 
+(defn create-cv [languageKey tags data] (cv-creator.html-renderer/create-html (languageKey data)))
+
 ; TODO: don't hardcode data file path
 ; TODO: don't hardcode language
-(defn -main [] (println (cv-creator.html-renderer/create-html
-                         (:english (cv-creator.deserializer/deserialize "sample_data_en.json")))))
+(defn -main [] (println
+                (create-cv :english [] (cv-creator.deserializer/deserialize "sample_data_en.json"))))
