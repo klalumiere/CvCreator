7144430ee63de47ccf5fbefb8ea018a359a1e36b
Kevin Lalumiere <kevin.lalumiere@gmail.com>
2024-09-28 13:52:27 -0400

    Add filter-tags

--- src/cv_creator/core.clj
+++ src/cv_creator/core.clj
@@ -1,6 +1,7 @@
 (ns cv-creator.core
   (:require
    [clojure.spec.test.alpha :as spectest]
+   [clojure.set]
    [clojure.string :as string]
 
    [cv-creator.deserializer]
@@ -16,6 +17,18 @@
   (spectest/instrument (spectest/enumerate-namespace 'cv-creator.deserializer)))
 
 (defn create-cv [language tags data] (cv-creator.html-renderer/create-html (:sections (language data))))
+
+(defn tags-in-common? [object tags]
+  (let [objectTags (get object :tags)]
+    (or (empty? objectTags) 
+        (seq? (clojure.set/intersection (set tags) (set objectTags))))))
+
+(defn filter-tags [data tags]
+  (cond
+    (vector? data) (let [updatedData (filter #(tags-in-common? % tags) data)]
+                    updatedData)
+    :else data
+    ))
 
 (defn -main [dataFolder language & rawTags]
   (let [tags (or rawTags [])]
--- test/cv_creator/core_test.clj
+++ test/cv_creator/core_test.clj
@@ -5,7 +5,31 @@
    [cv-creator.core :as core]
    [cv-creator.section :as section]))
 
-(test/deftest core
+(def a-tag "arbitrary")
+(def another-tag "anotherArbitrary")
+(def a-section-without-tags (section/create-section-from-map {:label "sectionLabel"
+                                                              :items [{:value "An item with subitems"
+                                                                       :subitems [{:value "An item"} {:value "An item"}]}]}))
+
+(def a-section-with-tags (section/create-section-from-map {:label "sectionLabel"
+                                                           :tags [a-tag]
+                                                           :items [{:value "An item with subitems"
+                                                                    :subitems [{:value "An item"} {:value "An item"}]}]}))
+
+(test/deftest filter-tags
+  (test/testing "filter-tags keep sections with same tags"
+    (test/is (= [a-section-with-tags] (core/filter-tags [a-section-with-tags] [a-tag]))))
+
+  (test/testing "filter-tags filters sections with different tags"
+    (test/is (= [] (core/filter-tags [a-section-with-tags] [another-tag]))))
+
+  (test/testing "filter-tags returns section if it has no tags"
+    (test/is (= [a-section-without-tags] (core/filter-tags [a-section-without-tags] []))))
+
+  (test/testing "filter-tags handles nill"
+    (test/is (= nil (core/filter-tags nil [])))))
+
+(test/deftest create-cv
   (test/testing "create-cv is not empty"
     (test/is (not (= "<div class=\"cvStyle\"></div>"
                      (core/create-cv :english [] {:english
