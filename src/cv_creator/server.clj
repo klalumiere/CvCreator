4daf28ade7d5c093b1b93521e2e48f0b9603ccba
Kevin Lalumiere <kevin.lalumiere@gmail.com>
2024-09-29 09:13:12 -0400

    Add non-trivial server

--- src/cv_creator/server.clj
+++ src/cv_creator/server.clj
@@ -1,10 +1,22 @@
 (ns cv-creator.server
+  #_{:clj-kondo/ignore [:deprecated-namespace]}
   (:require
    [compojure.core :as compojure]
+   [compojure.handler]
    [compojure.route :as route])
   (:gen-class))
 
-#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
-(compojure/defroutes app
+(compojure/defroutes trivial-impl
   (compojure/GET "/" [] "<h1>Hello World</h1>")
   (route/not-found ""))
+
+(compojure/defroutes app-impl
+  (compojure/GET "/cvcreator" [language tags]
+    (str "language=" language "tags=" tags))
+  (route/not-found ""))
+
+; I prefer using deprecated API than adding the new dependency
+; `ring-clojure/ring-defaults` with a version < 1 (with no new commit since 8 months)
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
+(def app #_{:clj-kondo/ignore [:deprecated-var]}
+          (-> app-impl compojure.handler/api))
