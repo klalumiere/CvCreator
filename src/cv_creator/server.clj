123432e858f354d849d7cf48fff200167347cf84
Kevin Lalumiere <kevin.lalumiere@gmail.com>
2024-09-29 09:33:13 -0400

    Skeleton for cvcreator server

--- src/cv_creator/server.clj
+++ src/cv_creator/server.clj
@@ -1,18 +1,28 @@
 (ns cv-creator.server
   #_{:clj-kondo/ignore [:deprecated-namespace]}
   (:require
+   [clojure.string :as string]
+   
    [compojure.core :as compojure]
    [compojure.handler]
-   [compojure.route :as route])
+   [compojure.route :as route]
+   
+   [cv-creator.core]
+   [cv-creator.deserializer])
   (:gen-class))
 
+(def cv-creator-data-dir-path (or (System/getenv "CV_CREATOR_DATA_PATH") "data/sample"))
+
+(def cv-creator-data (cv-creator.deserializer/deserialize-folder cv-creator-data-dir-path))
+
+; TODO: remove this when we're ready to deploy in production
 (compojure/defroutes trivial-impl
   (compojure/GET "/" [] "<h1>Hello World</h1>")
   (route/not-found ""))
 
 (compojure/defroutes app-impl
   (compojure/GET "/cvcreator" [language tags]
-    (str "language=" language "tags=" tags))
+    (cv-creator.core/create-cv (keyword language) (set (string/split tags #",")) cv-creator-data))
   (route/not-found ""))
 
 ; I prefer using deprecated API than adding the new dependency
