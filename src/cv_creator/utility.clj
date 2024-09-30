9bab75c1083a7fe7df59dd8f3164faf0cd9354b2
Kevin Lalumiere <kevin.lalumiere@gmail.com>
2024-09-29 21:18:43 -0400

    Refactor utility

--- src/cv_creator/utility.clj
+++ src/cv_creator/utility.clj
@@ -1,10 +1,9 @@
 (ns cv-creator.utility)
+(declare get-ordered-section-keys)
 
 (defn get-language-key [metadata] (keyword (:value (:language metadata))))
 
 (defn get-language-label [metadata] (:label (:language metadata)))
-
-(defn- get-ordered-section-keys [metadata] (map keyword (:order metadata)))
 
 (defn get-ordered-sections [metadata content] (mapv #(get content %) (get-ordered-section-keys metadata)))
 
@@ -12,3 +11,6 @@
 
 (defn update-if-exist [aMap key f]
   (if (nil? (key aMap)) aMap (update aMap key f)))
+
+
+(defn- get-ordered-section-keys [metadata] (map keyword (:order metadata)))
