a2366da9d90b805f400675051976ec821b46fd76
Kevin Lalumiere <kevin.lalumiere@gmail.com>
2024-11-02 09:15:57 -0400

    Remove Ruby

--- systemTests.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-./run.sh HtmlView data En research computerScience teaching other > build/result.html
-./run.sh TexView data En research computerScience teaching other > build/result.tex
